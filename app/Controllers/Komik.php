755528b4f1dc764d15e40c833afac2e606cfd93f
charint4 <rafifridhoo@gmail.com>
2021-06-15 08:59:27 +0700

    update and delete function

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -77,4 +77,60 @@ class Komik extends BaseController
 
     return redirect()->to('/komik');
   }
+
+  public function delete($id)
+  {
+    $this->komikModel->delete($id);
+    session()->setFlashdata('message', 'Comic deleted succesfully.');
+    return redirect()->to('/komik');
+  }
+
+  public function edit($slug)
+  {
+    $data = [
+      'title' => 'Edit Comic Form',
+      'validation' => \Config\Services::validation(),
+      'komik' => $this->komikModel->getKomik($slug),
+    ];
+
+    return view('komik/edit', $data);
+  }
+
+  public function update($id)
+  {
+    //cek title
+    $komikLama = $this->komikModel->getKomik($this->request->getVar('slug'));
+    if($komikLama['title'] == $this->request->getVar('title')){
+      $rules_title = 'required';
+    } else {
+      $rules_title = 'required|is_unique[komik.title]';
+    }
+
+    if(!$this->validate([
+      'title' => [
+        'rules' => $rules_title,
+        'errors' => [
+          'required' => 'Comic {field} needs to be filled',
+          'is_unique' => 'Comic {field} must be unique',
+        ]
+      ],
+    ])) {
+      $validation = \Config\Services::validation();
+      return redirect()->to('/komik/edit/'. $this->request->getVar('slug'))->withInput()->with('validation', $validation);
+    }
+
+    $slug = url_title($this->request->getVar('title'), '-', true);
+    $this->komikModel->save([
+      'id' => $id,
+      'title' => $this->request->getVar('title'), 
+      'slug' => $slug,
+      'author' => $this->request->getVar('author'), 
+      'publisher' => $this->request->getVar('publisher'), 
+      'cover' => $this->request->getVar('cover'), 
+    ]);
+
+    session()->setFlashData('message', 'Comic updated succesfully.');
+
+    return redirect()->to('/komik');
+  }
 }
--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -49,10 +49,10 @@
 <?= $this->section('content'); ?>
 <div class="container pb-5">
   <div class="header">
-    <h1 class="welcome text-center my-5">Comic Detail</h1>
-    <div class="container content" style="padding: 0 200px"> 
+    <h1 class="my-5 text-center welcome">Comic Detail</h1>
+    <div class="container px-5 content"> 
       <div class="row">
-        <div class="col-md-12 my-3 p-4 d-flex justify-content-center">
+        <div class="p-4 my-3 col-md-12 d-flex justify-content-center">
           <!-- <div class="card" style="width: 18rem;"> -->
             <img src="/img/<?= $komik['cover']; ?>" class="card-img-top" alt="...">
             <!-- </div> -->
@@ -60,34 +60,38 @@
         </div>
         <div class="content">
           <div class="row">
-            <div class="col-md-12 d-flex flex-row justify-content-center">
+            <div class="flex-row col-md-12 d-flex justify-content-center">
               <h5 class="card-title"><?= $komik['title']; ?></h5>
             </div>
           </div>
           <div class="row">
-            <div class="col-md-12 mt-4 d-flex flex-row justify-content-center">
+            <div class="flex-row mt-4 col-md-12 d-flex justify-content-center">
               <p class="card-text"><?= $komik['author']; ?></p>
             </div>
           </div>
           <div class="row">
-            <div class="col-md-12 m-2 d-flex flex-row justify-content-center">
+            <div class="flex-row m-2 col-md-12 d-flex justify-content-center">
               <p class="card-text"><?= $komik['publisher']; ?></p>
             </div>
           </div>
           <div class="row">
-            <div class="col-md-12 m-4 d-flex flex-row justify-content-center">
+            <div class="flex-row m-4 col-md-12 d-flex justify-content-center">
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
           <div class="row">
-            <div class="col-md-12 m-4 d-flex flex-row justify-content-center">
-              <a href="" class="btn">edit</a>
-              <a href="" class="btn">delete</a>
+            <div class="flex-row m-4 col-md-12 d-flex justify-content-center">
+              <a href="/komik/edit/<?= $komik['slug']; ?>" class="btn">edit</a>
+              <form action="/komik/<?= $komik['id']; ?>" method="post">
+              <input type="hidden" name="_method" value="DELETE">
+                <?= csrf_field(); ?>
+                <button type="submit" class="btn" onclick="return confirm('Are you sure you want to delete this comic?')">delete</button>
+              </form>
               <br>
             </div>
           </div>
         </div>
-        <a href="/komik" class="btn m-4">Return</a>
+        <a href="/komik" class="m-4 btn">Return</a>
       </div>
   </div>
 </div>
